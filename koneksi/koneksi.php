cc20b984c1ab1ce1a25a123d7b4ed383074aab99
Leaticia-ai <80943788+Leaticia-ai@users.noreply.github.com>
2021-03-19 14:37:28 +0700

    Add files via upload

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,2 +1,12 @@
-<?php
+<?php 
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$db = "logic_gc";
+
+	$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+	if(!$koneksi) {
+		die("Koneksi gagal : ".mysqli_connect_error());
+	}
 ?>
